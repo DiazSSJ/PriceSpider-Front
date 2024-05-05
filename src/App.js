851e67b9f304927f71c93e838a7e1ee8a2c67b4e
DiazSSJ <juans220304@gmail.com>
2024-05-05 00:40:45 -0500

    HU03 HU04-Consumir el servicio de back y mostrar los resultado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, FormControl, Button } from 'react-bootstrap';
 import ProductCard from './componentes/product_card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import logo from './Resources/logo.png'
+import logo from './Resources/logo.png';
+import LoadingSpinner from './componentes/Loading/Loading';
 
 const App = () => {
-  // Función para generar un array de 10 elementos (puedes usar Array(10).fill(0) para generar un array)
-  const productCards = Array.from({ length: 0 }, (_, index) => (
-    <ProductCard key={index} className="product-card" />
-  ));
+  const [productName, setProductName] = useState('');
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSearch = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('http://127.0.0.1:5000/scraping', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          nombre_producto: productName
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setProducts(data);
+        setIsLoading(false);
+      } else {
+        console.error('Error al realizar la solicitud al backend:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud al backend:', error);
+    }
+  };
 
   return (
     <div className="App">
+      <LoadingSpinner isLoading={isLoading} /> {/* Usar el componente LoadingSpinner */}
       <div className='contenedor-padre d-flex justify-content-center'>
         {/* Agregar el título y el logo */}
         <div className="titulo-y-logo mb-4">
@@ -25,10 +51,16 @@ const App = () => {
         {/* Barra de búsqueda */}
         <div className="search-bar mb-4">
           <p>Encuentra las mejores ofertas en productos tecnológicos en línea. Utilizando técnicas avanzadas de web scraping, PriceSpider rastrea múltiples tiendas en línea y recopila información detallada sobre los precios de una amplia gama de productos tecnológicos, como teléfonos móviles, computadoras portátiles, tabletas y dispositivos electrónicos.</p>
-          <Form inline>
+          <Form inline onSubmit={e => e.preventDefault()}>
             <div className="search-input-container">
-              <FormControl type="text" placeholder="Ingresa el producto que deseas buscar" className="mr-sm-2" />
-              <Button variant="link" className="search-button">
+              <FormControl
+                type="text"
+                placeholder="Ingresa el producto que deseas buscar"
+                className="mr-sm-2"
+                value={productName}
+                onChange={e => setProductName(e.target.value)}
+              />
+              <Button variant="link" className="search-button" onClick={handleSearch}>
                 <FontAwesomeIcon icon={faSearch} />
               </Button>
             </div>
@@ -36,7 +68,16 @@ const App = () => {
         </div>
         {/* Componente de productos */}
         <div className="product-cards-container">
-          {productCards}
+          {products.map((product, index) => (
+            <ProductCard
+              key={index}
+              imagen={product.img}
+              nombre={product.nombre}
+              precio={product.precio}
+              tienda={product.tienda}
+              url={product.url}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/componentes/product_card.jsx
+++ src/componentes/product_card.jsx
@@ -1,35 +1,56 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './product_card.css'
-import carIcon from '../Resources/car.png'
+import './product_card.css';
+import carIcon from '../Resources/car.png';
+
+function truncateText(text, maxLength) {
+    if (text.length > maxLength) {
+        return text.substring(0, maxLength - 3) + '...';
+    }
+    return text;
+}
+
+function formatPrecio(precio) {
+    return precio.replace('COP', '$').replace(/\s/g, ''); // Elimina los espacios en blanco y reemplaza "COP" con "₱"
+}
 
 
-function ProductCard() {
+function ProductCard({ nombre, tienda, precio, imagen, url }) {
+    const redirectToStore = () => {
+        window.open(url, '_blank');
+    };
+
+    const truncatedNombre = truncateText(nombre, 25); // Trunca el nombre a 20 caracteres
+    const truncatedTienda = truncateText(tienda, 25); // Trunca el nombre de la tienda a 20 caracteres
+    const truncatedPrecio = formatPrecio(precio); // No se trunca el precio
+
     return (
-        <div className="container-page p-3" >
-            <div className="container-img mb-2  ">
-                <img src="https://primefaces.org/cdn/primereact/images/landing/air-jordan.png" alt="Watch" className="img" />
+        <div className="container-page p-3">
+            <div className="container-img mb-2">
+                <img src={imagen} alt="Watch" className="img" />
+            </div>
+            <div className="row mb-2">
+                <div className="col">
+                    <span className="block font-semibold" style={{ maxHeight: '2.4em', overflow: 'hidden' }}>{truncatedNombre}</span>
+                </div>
+            </div>
+            <div className="row mb-2">
+                <div className="col">
+                    <span className="text-secondary" style={{ maxHeight: '1.2em', overflow: 'hidden' }}>{truncatedTienda}</span>
+                </div>
             </div>
             <div className="row mb-4">
                 <div className="col">
-                    <div className='row'>
-                        <span className="block font-semibold">Nombre producto</span>
-                        <span className="text-secondary">Tienda producto</span>
-                    </div>
-                </div>
-                <div className="col-4 d-flex justify-content-end align-items-center">
-                    <span className="font-weight-bold fs-3">$990</span>
+                    <span className="font-weight-bold fs-5">{truncatedPrecio}</span>
                 </div>
             </div>
 
-            <button className="button-card p-2 font-weight-bold">
+            <button className="button-card p-2 font-weight-bold" onClick={redirectToStore}>
                 <div><img src={carIcon} className="mr-2 icon-button-card" /></div>
                 <span className="fs-6">Ver en la tienda</span>
             </button>
-
         </div>
     );
-
 }
 
 export default ProductCard;
